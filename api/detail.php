28549298d1f09be84b9f0f753d1fff3ae7639d27
Hunterplant01 <hattaziyad@gmail.com>
2026-08-27 04:33:55 +0700

    Update detail.php

--- api/detail.php
+++ api/detail.php
@@ -111,5 +111,6 @@ $imgSrc = (strpos($p['image'], 'http') === 0) ? $p['image'] : "/assets/img/produ
     </div>
   </div>
 </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
